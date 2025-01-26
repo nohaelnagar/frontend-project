d0cde17624eda974a5ddc2ce66ea22bb562442d8
nohaelnagar <eng.noha17@gmail.com>
2025-01-26 09:01:58 +0100

    Error Handling

--- quick-headlines/src/App.jsx
+++ quick-headlines/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import HeadlinesHeader from "./Component/HeadlinesHeader";
 import HeadlinesFeed from "./Component/HeadlinesFeed";
-import { debounce } from "lodash";
+import { debounce, set } from "lodash";
 import { styled } from "@mui/system";
 
 const FooterButtons = styled("div")(({ theme }) => ({
@@ -11,20 +11,26 @@ const FooterButtons = styled("div")(({ theme }) => ({
   justifyContent: "space-between",
 }));
 
+const PAGE_SIZE = 5;
+
 function App() {
   const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const pageNo = useRef(1);
   const queryValue = useRef("");
 
   async function loadData() {
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?q=${queryValue.current}&page=${pageNo.current}&pageSize=5&country=us&apiKey=${
-        import.meta.env.VITE_NEWS_API_KEY
+      `https://newsapi.org/v2/top-headlines?q=${queryValue.current}&page=${pageNo.current
+      }&pageSize=${PAGE_SIZE}&country=us&apiKey=${import.meta.env.VITE_NEWS_API_KEY
       }`
     );
 
     const data = await response.json();
+    if (data.status === "error") {
+      throw new Error("An Error has Occured");
+    }
     return data.articles.map(article => {
       const { title, description, author, publishedAt, urlToImage } = article;
       return {
@@ -33,23 +39,29 @@ function App() {
         author,
         publishedAt,
         image: urlToImage,
-      }
+      };
     });
-  }
+  } 
+
+
 
   const getFetchHeadlines = () => {
     setLoading(true);
+    setError("");
     loadData().then((newData) => {
       setArticles(newData);
+    }).catch(errorMessage => {
+      setError(errorMessage.message);
+    }).finally(() => {
       setLoading(false);
     });
   };
 
 
   const debouncedData = debounce(getFetchHeadlines, 500);
-
+  
   useEffect(() => {
-    getFetchHeadlines()
+    getFetchHeadlines();
   }, []);
 
   const searchChange = (newQeury) => {
@@ -64,17 +76,35 @@ function App() {
   };
 
   const previousClick = () => {
-    pageNo.current -= 1;
-    getFetchHeadlines();
+      pageNo.current -= 1;
+      getFetchHeadlines();
   };
 
   return (
     <Container>
       <HeadlinesHeader onSearchChange={searchChange} />
-      <HeadlinesFeed articles={articles} loading={loading} />
+      {error.length === 0 ? (
+        <HeadlinesFeed articles={articles} loading={loading} />
+      ) : (
+        <Typography color="error" align="center">
+          {error}
+        </Typography>
+      )}
       <FooterButtons>
-        <Button variant="outlined" onClick={previousClick}>Previous</Button>
-        <Button variant="outlined" onClick={nextClick}>Next</Button>
+        <Button
+          variant="outlined"
+          onClick={previousClick}
+          disabled={pageNo.current === 1}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={nextClick}
+          disabled={articles.length < PAGE_SIZE}
+        >
+          Next
+        </Button>
       </FooterButtons>
     </Container>
   );
